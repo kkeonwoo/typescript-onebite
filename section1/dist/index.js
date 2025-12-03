f18b64cd54396d687e3316a9bd8f16d839c1af83
Lee keonwoo <110362115+kkeonwoo@users.noreply.github.com>
2025-12-03 13:08:10 +0900

    onebite typescript / 원시타입

---
+++ section1/dist/index.js
@@ -0,0 +1,5 @@
+console.log('Hello TypeScript');
+const a = 1;
+const func = () => console.log('Hello');
+export {};
+// 독립된 걸로 봄
---
+++ section1/src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hello TypeScript');
+const a: number = 1;
+const func = () => console.log('Hello');
+
+export {} // 파일에 export 모듈 키워드가 있으면
+// 독립된 걸로 봄
